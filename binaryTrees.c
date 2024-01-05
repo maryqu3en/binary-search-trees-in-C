26cec77ddbe32afaefb2f28bea2e422aebfd00d3
Meriem Soubih <soubih.meriem@gmail.com>
2024-01-05 10:57:07 +0100

    updated balanced tree from sorted array

--- binaryTrees.c
+++ binaryTrees.c
@@ -227,3 +227,18 @@ void printLeafNodes(treeNode* root) {
     printLeafNodes(root->right);
 }
 
+
+treeNode* sortedArrayToBST(int arr[], int start, int end) {
+    if (start > end) {
+        return NULL;
+    }
+
+    int mid = (start + end) / 2;
+    treeNode* node = (treeNode*)malloc(sizeof(treeNode));
+
+    node->data = arr[mid];
+    node->left = sortedArrayToBST(arr, start, mid - 1);
+    node->right = sortedArrayToBST(arr, mid + 1, end);
+
+    return node;
+}
